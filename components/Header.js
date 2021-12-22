7ab61ef22acd08dea898400fa935ea8c4b116640
Carlo Francisco <carlofrancisco@Carlos-MacBook-Pro.local>
2021-12-21 19:04:08 -0500

    sample header done

---
+++ components/Header.js
@@ -0,0 +1,29 @@
+import {
+    BadgeCheckIcon, 
+    CollectionIcon, 
+    HomeIcon, 
+    LightningBoltIcon,
+    SearchIcon, 
+    UserIcon
+} from "@heroicons/react/outline"
+import Image from "next/image"
+import HeaderItem from "../pages/HeaderItem"
+
+
+function Header() {
+    return (
+        <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+            <div className="flex flex-grow justify-evenly max-w-2xl">
+                <HeaderItem title="HOME" Icon={HomeIcon} />
+                <HeaderItem title="TRENDING" Icon={BadgeCheckIcon} />
+                <HeaderItem title="VERIFIED" Icon={CollectionIcon} />
+                <HeaderItem title="COLLECTIONS" Icon={LightningBoltIcon} />
+                <HeaderItem title="SEARCH" Icon={SearchIcon} />
+                <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+            </div>
+            <h1 className="object-contain">Video Streamer LOGO</h1>
+        </header>
+    )
+}
+
+export default Header
