e6dec8090f4e784764fd195b757762d51e9b6ede
Carlo Francisco <carlofrancisco@Carlos-MacBook-Pro.local>
2021-12-23 12:44:50 -0500

    nav scrolling

--- components/Header.js
+++ components/Header.js
@@ -1,29 +1,28 @@
 import {
-    BadgeCheckIcon, 
-    CollectionIcon, 
-    HomeIcon, 
-    LightningBoltIcon,
-    SearchIcon, 
-    UserIcon
-} from "@heroicons/react/outline"
-import Image from "next/image"
-import HeaderItem from "../pages/HeaderItem"
-
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+import Image from "next/image";
+import HeaderItem from "../pages/HeaderItem";
 
 function Header() {
-    return (
-        <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
-            <div className="flex flex-grow justify-evenly max-w-2xl">
-                <HeaderItem title="HOME" Icon={HomeIcon} />
-                <HeaderItem title="TRENDING" Icon={BadgeCheckIcon} />
-                <HeaderItem title="VERIFIED" Icon={CollectionIcon} />
-                <HeaderItem title="COLLECTIONS" Icon={LightningBoltIcon} />
-                <HeaderItem title="SEARCH" Icon={SearchIcon} />
-                <HeaderItem title="ACCOUNT" Icon={UserIcon} />
-            </div>
-            <h1 className="object-contain">Video Streamer LOGO</h1>
-        </header>
-    )
+  return (
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={BadgeCheckIcon} />
+        <HeaderItem title="VERIFIED" Icon={CollectionIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={LightningBoltIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
+      <h1 className="object-contain">Video Streamer LOGO</h1>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
