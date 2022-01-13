27d2947f935ece18da95fac4028c33d9e944d097
Carlo Francisco <carlofrancisco@Carlos-MacBook-Pro.local>
2022-01-13 15:35:43 -0500

    moved contentID page into content component

--- components/Content.js
+++ components/Content.js
@@ -1,6 +1,34 @@
 import { useRouter } from "next/router";
 
 const Content = ({ content }) => {
-  <div></div>;
+  let youtubeKey;
+  let youtubeName;
+  let youtubeLink;
+  console.log("content", content);
+
+  if (content.results[0] == null) {
+    return (
+      <div className="flex justify-center m-20 text-2xl">
+        <h1>Trailer not available</h1>
+      </div>
+    );
+  } else {
+    let youtubeKey = content.results[0].key;
+
+    let youtubeName = content.results[0].name;
+    let youtubeLink = `https://www.youtube.com/embed/${youtubeKey}`;
+    return (
+      <div>
+        <div className="h-80v p-10 flex justify-center ">
+          <iframe
+            src={youtubeLink}
+            frameBorder="0"
+            height="100%"
+            width="100%"
+          ></iframe>
+        </div>
+      </div>
+    );
+  }
 };
 export default Content;
--- pages/content/[contentId].js
+++ pages/content/[contentId].js
@@ -4,35 +4,11 @@ import { useRouter } from "next/router";
 import Content from "../../components/Content";
 
 export default function ContentId({ content }) {
-  let youtubeKey;
-  let youtubeName;
-  let youtubeLink;
-  console.log("content", content);
-
-  if (content.results[0] == null) {
-    return (
-      <div className="flex justify-center m-20 text-2xl">
-        <h1>Trailer not available</h1>
-      </div>
-    );
-  } else {
-    let youtubeKey = content.results[0].key;
-
-    let youtubeName = content.results[0].name;
-    let youtubeLink = `https://www.youtube.com/embed/${youtubeKey}`;
-    return (
-      <div>
-        <div className="h-80v p-10 flex justify-center ">
-          <iframe
-            src={youtubeLink}
-            frameBorder="0"
-            height="100%"
-            width="100%"
-          ></iframe>
-        </div>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Content content={content} />
+    </div>
+  );
 }
 
 export async function getServerSideProps(context) {
